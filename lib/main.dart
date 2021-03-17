12beeae8ce22da349327dcae3affa735ed7810d9
manirahmanzadeh <mani.rahmanzade@gmail.com>
2021-03-17 13:04:30 +0330

    stful

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-void main() {
-  runApp(
-      MaterialApp(
-        home: HomePage(),
-      )
-  );
-}
+
+void main()=> runApp(
+    MaterialApp(
+      home: HomePage(),
+    )
+);
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,40 +13,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  String centerText = "empty";
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(centerText),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getData,
-      )
-    );
+    return Container();
   }
-
-  getData()async{
-    try{
-      final response = await http.get("http://192.168.1.53:3000/students")
-          .timeout(Duration(seconds: 10));
-      if(response.statusCode==200){
-        setState(() {
-          centerText = response.body;
-        });
-      }else{
-        setState(() {
-          centerText = "Something went wrong";
-        });
-      }
-    }catch(e){
-      setState(() {
-        centerText = "Something went wrong";
-      });
-    }
-  }
-
-
 }
+
+
